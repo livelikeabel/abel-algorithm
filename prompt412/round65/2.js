693071ac931fc613e67318f313bbc5fea39a1530
abelko <esung1129@gmail.com>
2020-02-22 16:11:22 +0900

    solve Chocolate

--- prompt412/round65/2.js
+++ prompt412/round65/2.js
@@ -16,8 +16,35 @@ rl.on('line', function (line) {
 
 function func(input) {
     const barLength = input[0];
-    const bar = input[1];
-    console.log(bar);
+    const bar = input[1].split(' ');
+    const lastOne = bar.lastIndexOf('1');
+    const data = bar.reduce((acc, v, i) => {
+        if (v === '1' && !acc.isStart) {
+            acc.isStart = true;
+            acc.zeroSection++;
+        } else if (i === lastOne) {
+            acc.isStart = false;
+        } else if (v === '1' && acc.isStart) {
+            acc.zeroSection++;
+        } else if (v === '0' && acc.isStart) {
+            if (acc[acc.zeroSection]) {
+                acc[acc.zeroSection] += 1
+            } else {
+                acc[acc.zeroSection] = 1
+            }
+        }
+        return acc
+    }, {
+        isStart: false,
+        zeroSection: 0
+    });
 
-    return '';
+    if(data.zeroSection === 1) return 1;
+
+    const tempArr = [];
+    for (let i = 1; i <= data.zeroSection; i++) {
+        tempArr.push(data[i] + 1);
+    }
+
+    return tempArr.reduce((acc, v) => acc * v);
 }
